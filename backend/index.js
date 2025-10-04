6f2798dee0e35d4a4da19257cb9e74281a296729
molnarimi0211 <molnarimi0211@gmail.com>
2025-10-04 15:20:36 +0200

    Using more precise error output in the backend.

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
+const client = require('prom-client');
 require("dotenv").config();
 
+const collectMetrics = client.collectDefaultMetrics;
+collectMetrics();
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool.connect(async (err, client, release) => {
+  if (err) {
+    return console.error("Error connecting to the database:", err.stack);
+  }
+  console.log("Successfully connected to the database!");
+//});
+
+
+  try {
+    const result = await client.query("SELECT * FROM games");
+    console.log("Data fetched on successful connection:");
+    console.log(result.rows);
+  } catch (queryErr) {
+    console.error("Error fetching data immediately after connection:", queryErr);
+  } finally {
+    release(); 
+  }
+});
+
+app.get('/metrics', (req, res) =>{
+  res.set('Content-Type', client.register.contentType);
+  res.send(client.register.metrics());
+})
 
 
 app.get("/api/games", async (req, res) => {
@@ -23,8 +52,8 @@ app.get("/api/games", async (req, res) => {
     const result = await pool.query("SELECT * FROM games");
     res.json(result.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error during data fetch" });
+    console.error("Error running query:", err);
+    res.status(500).json({ error: err.message });
   }
 });
 
