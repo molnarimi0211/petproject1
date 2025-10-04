099b1e12887939cbd1e525b4636096971a775daa
molnarimi0211 <molnarimi0211@gmail.com>
2025-10-04 15:31:15 +0200

    Added key_name to the terraform variables.

--- terraform/var.tf
+++ terraform/var.tf
@@ -1,6 +1,11 @@
-# Külön változó a jelszónak
 variable "db_password" {
   description = "The password for the RDS PostgreSQL user"
   type        = string
   sensitive   = true
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+  
 }
